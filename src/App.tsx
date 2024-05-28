19cbb5e3699fef2ad7f3700e2c2e0c064872dbaf
alex-surmylo <alexbal2010@gmail.com>
2024-05-28 21:37:10 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
+import NextButton from "./components/NextButton";
+import Progress from "./components/Progress";
+import FinishScreen from "./components/FinishScreen";
+import Footer from "./components/Footer";
+import Timer from './components/Timer'
+import { useQuiz } from "./context/QuizContext";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App () {
+    const { status }  = useQuiz ();
+
+    return (
+        <div className="app">
+            <Header/>
+
+            <Main>
+                { status === 'loading' && <Loader/> }
+                { status === 'error' && <Error/> }
+                { status === 'ready' && <StartScreen/> }
+                { status === 'active' && (
+                  <>
+                    <Progress/>
+                    <Question/>
+                    <Footer>
+                      <Timer/>
+                      <NextButton/>
+                    </Footer>
+                  </>
+                )}
+                { status === 'finished' && <FinishScreen/> }
+            </Main>
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { QuizProvider } from "./context/QuizContext";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+const root = ReactDOM.createRoot (
+    document.getElementById ( 'root' ) as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+root.render (
+    <React.StrictMode>
+        <QuizProvider>
+            <App/>
+        </QuizProvider>
+    </React.StrictMode>
 );
